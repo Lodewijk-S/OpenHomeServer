d8cc8feb98565ee7d0700251a4045438aef6259c
Lodewijk Sioen <lodewijksioen@gmail.com>
2014-03-23 20:53:41 +0100

    Let's not do javascript like it's 1999

---
+++ src/OpenHomeServer.Server/Web/Assets/Scripts/OpenHomeServer.js
@@ -0,0 +1,22 @@
+﻿var openHomeServer = function(){
+	me = this;
+	me.notificator = new notificator();	
+};
+
+var notificator = function(){
+	me = this;
+	toastr.options.closeButton = true;
+	var notificationHub = $.connection.notificationHub;
+	
+	me.sendNotification = function(message, link) {
+        notificationHub.server.notifyAll({ Message: message, Link: link });
+    }
+     
+    notificationHub.client.onRecieveNotification = function (notification) {
+        toastr.info(notification.Message + "<br /><a href='"+notification.Link+"'>Click here for more info</a>", "Notification");
+    };
+	 
+    $.connection.hub.start()
+        .done(function () { console.log('Now connected, connection ID=' + $.connection.hub.id); })
+        .fail(function () { console.log('Could not Connect!'); });     
+}
