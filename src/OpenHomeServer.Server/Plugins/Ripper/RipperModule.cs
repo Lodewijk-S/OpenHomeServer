699b5bdfe72567da7747cf7f7d3fc1039058e409
Lodewijk Sioen <lodewijksioen@gmail.com>
2014-04-06 21:31:25 +0200

    Starting the UI for the Ripper Plugin

--- src/OpenHomeServer.Server/Plugins/Ripper/RipperModule.cs
+++ src/OpenHomeServer.Server/Plugins/Ripper/RipperModule.cs
@@ -4,11 +4,25 @@ namespace OpenHomeServer.Server.Plugins.Ripper
 {
     public class RipperModule : NancyModule
     {
-        public RipperModule()
+        public RipperModule(RipperService service)
             : base("ripper")
         {
-            Get["/"] = x => View["index.cshtml"];
+            Get["/"] = x => View["index.cshtml", GetRipperViewModel(service)];
         }
 
-    }    
+        private RipperViewModel GetRipperViewModel(RipperService service)
+        {
+            return new RipperViewModel
+            {
+                Title="Ripper",
+                Status = service.GetCurrentStatus()
+            };
+        }
+    }
+
+    public class RipperViewModel
+    {
+        public string Title { get; set; }
+        public RippingStatus Status { get; set; }
+    }
 }
